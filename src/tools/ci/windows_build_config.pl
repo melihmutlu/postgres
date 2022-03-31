4a3326d42cf081d2efd90db82709d176601a0f89
Melih Mutlu <m.melihmutlu@gmail.com>
2022-03-31 14:20:22 +0300

    Pg dependencies for windows with VS19 ci-os-only:windows

--- src/tools/ci/windows_build_config.pl
+++ src/tools/ci/windows_build_config.pl
@@ -6,6 +6,10 @@ our $config;
 $config->{"tap_tests"} = 1;
 $config->{"asserts"} = 1;
 
+$config->{"icu"} = 1;
+$config->{"lz4"} = 1;
+$config->{"zstd"} = 1;
+
 $config->{"openssl"} = "c:/openssl/1.1/";
 $config->{"perl"} = "c:/strawberry/$ENV{DEFAULT_PERL_VERSION}/perl/";
 $config->{"python"} = "c:/python/";
