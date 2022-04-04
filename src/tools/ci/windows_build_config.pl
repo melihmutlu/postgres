bb5a7a80a5e48b184fa4a663c100b639757f6828
Melih Mutlu <m.melihmutlu@gmail.com>
2022-04-04 14:05:52 +0300

    added path for additional deps
    ci-os-only:windows

--- src/tools/ci/windows_build_config.pl
+++ src/tools/ci/windows_build_config.pl
@@ -6,9 +6,9 @@ our $config;
 $config->{"tap_tests"} = 1;
 $config->{"asserts"} = 1;
 
-$config->{"icu"} = 1;
-$config->{"lz4"} = 1;
-$config->{"zstd"} = 1;
+$config->{"icu"} = "c:/icu/";
+$config->{"lz4"} = "c:/lz4";
+$config->{"zstd"} = "c:/zstd/zstd-v1.5.2-win64";
 
 $config->{"openssl"} = "c:/openssl/1.1/";
 $config->{"perl"} = "c:/strawberry/$ENV{DEFAULT_PERL_VERSION}/perl/";
